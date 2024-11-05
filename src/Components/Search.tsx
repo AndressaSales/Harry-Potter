0482ec4ae500a5688c9b77c98fb912cfa5478189
AndressaSales <andressaccs2001@gmail.com>
2024-11-05 14:25:58 -0300

    finalizando o projeto do Harry potter

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -8,6 +8,7 @@ export default function Search(){
         const response = await fetch(`https://potterapi-fedeperin.vercel.app/es/characters?search=${person}`)
         const caracteristicas = await response.json()
         setSearchCaracteres(caracteristicas)
+        console.log(caracteristicas)
     }
 
     return(
@@ -15,7 +16,27 @@ export default function Search(){
             <div>
                 <div className="items-center p-4 flex justify-center lg:flex-row flex-col">
                 <input type="text" placeholder="Ex: Harry Potter" className="lg:flex-grow w-[300px] text-2xl lg:text-[20px] outline-none border-[3px] border-[#f00] border-t-transparent border-r-transparent border-l-transparent " value={person} onChange={(e)=> setPerson(e.target.value)}/>
-                <button className="m-4 bg-sky-700 text-center text-[#fff] px-4 py-2 rounded-md text-xl">Buscar</button>
+                <button 
+                    className="m-4 bg-sky-700 text-center text-[#fff] px-4 py-2 rounded-md text-xl"
+                    onClick={buscar}
+                >Buscar</button>
+                </div>
+
+                <div className="justify-center items-center flex pb-[30px]">
+                    {
+                        searchCaracteres.map((characters)=>{
+                            return(
+                                <div key={characters.index}>
+                                    <div className="justify-center items-center flex">
+                                        <img src={characters.image} alt={characters.fullName} className="rounded-xl h-[340px] cursor-pointer duration-200 hover:scale-110" />
+                                    </div>
+                                    <p className="text-2xl p-2 font-bold text-center">{characters.fullName}</p>
+                                    <p className="font-bold text-center">{characters.hogwartsHouse}</p>
+                                    <p className="text-center font-mono ">{characters.birthdate}</p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </>
